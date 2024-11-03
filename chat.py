46e0b501f9459698f02f1628b64d60436ca456f4
Skillergp <95884100+Skillergp@users.noreply.github.com>
2024-11-03 11:31:11 +0100

    Chatbot Startbildschirm Test

--- chat.py
+++ chat.py
@@ -1,2 +1,8 @@
 # Importieren der Bibliotheken
-from ctransformers import AutoModelForCausalLM
+
+import chainlit as cl
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = f"Hello, you just sent: {message.content}!"
+    await cl.Message(response).send()
